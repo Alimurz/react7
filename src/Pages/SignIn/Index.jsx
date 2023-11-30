439ed415a48d62be97ce8e3f393bceedff4d339a
Alimurz <148546039+Alimurz@users.noreply.github.com>
2023-11-30 10:31:56 +0600

    Update Index.jsx

--- src/Pages/SignIn/Index.jsx
+++ src/Pages/SignIn/Index.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import Input from "../../Components/Input/index.jsx";
 const Button = styled.button`
     padding: 10px;
-    background-color:rgb(0, 106, 255);
+    background-color:${(props) => props.backgroundColor};
     font-size: 20px;
     font-weight:bold;
     color:#fff;
@@ -35,7 +35,54 @@ function SignIn(){
             <input type="checkbox" className={Styles['forms-input1']}/>
             <label className={Styles["sign-label"]}>Remember me</label>
             </div>
-            <Button className={Styles['sign_in_button']}>Sign In</Button>
+            <Button className={Styles['sign_in_button']}
+            backgroundColor='red'
+            >Sign In</Button>
+        </form>
+     </div>
+        
+    )
+}
+export default SignIn;import React from "react";
+import Styles from '../../App.module.scss'
+import styled from "styled-components";
+import Input from "../../Components/Input/index.jsx";
+const Button = styled.button`
+    padding: 10px;
+    background-color:${(props) => props.backgroundColor};
+    font-size: 20px;
+    font-weight:bold;
+    color:#fff;
+    border-radius: 25px;
+    margin-top:10px;
+    `
+    
+function SignIn(){
+    
+    
+    return(
+    <div className={Styles['center']}>
+        <form className={Styles['sign']}>
+            <img src="https://static-00.iconduck.com/assets.00/user-avatar-icon-512x512-vufpcmdn.png" alt="avatar" className={Styles['avatar-icon']} />
+            <div className={Styles['forms-item']}>
+                <label className={Styles['inputs-label']}>Username</label>
+                <Input 
+                type='text' 
+                placeholder="Write your Username..."
+                className={Styles['forms-input']}
+                />
+            </div>
+            <div className={Styles['forms-item1']}>
+                <label className={Styles['inputs-label']}>Password</label>
+                <input type="password" placeholder="Write your password..." className={Styles['forms-input']}/>
+            </div>
+            <div>
+            <input type="checkbox" className={Styles['forms-input1']}/>
+            <label className={Styles["sign-label"]}>Remember me</label>
+            </div>
+            <Button className={Styles['sign_in_button']}
+            backgroundColor='red'
+            >Sign In</Button>
         </form>
      </div>
         
